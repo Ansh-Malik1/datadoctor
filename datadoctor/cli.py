d50b63c64c1cc0e769c905a03a3639090e6f37e6
Ansh Malik <anshmalik718@gmail.com>
2025-07-23 23:34:47 +0530

    practicing adding multiple commands (good to go now with main project)

--- datadoctor/cli.py
+++ datadoctor/cli.py
@@ -10,3 +10,12 @@ def cli():
 def hello(name):
     """Say hello!"""
     click.echo(f"Hello, {name}!")
+    
+@cli.command()
+@click.option('--name',default='world',help='this a function to say bye')
+def bye(name):
+    click.echo(f"Bye {name}, It was a nice time having you!")
+    
+
+if __name__ == "__main__":
+    cli()
