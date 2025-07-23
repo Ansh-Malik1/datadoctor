2f0f7f9072bc9019c14c83d0dc8d3d03c2d5ad62
Ansh Malik <anshmalik718@gmail.com>
2025-07-23 23:54:37 +0530

    Initialized CLI structure and created placeholder for cleaner.py

---
+++ datadoctor/cleaner.py
@@ -0,0 +1,10 @@
+'''
+This is the file wherein the code to clean the csv will be written.
+As of now the objectives of this 'cleaner' will be :
+1. Drop null values
+2. Drop duplicate rows
+3. standardize the data (lowercase, remove spaces etc)
+4. Generate the output
+'''
+def clean_csv():
+    return
--- datadoctor/cli.py
+++ datadoctor/cli.py
@@ -1,20 +1,19 @@
 import click
-
+from datadoctor.cleaner import clean_csv
 @click.group()
 def cli():
     """Datadoctor CLI"""
     pass
-
-@cli.command()
-@click.option('--name', default='world', help='Name to greet')
-def hello(name):
-    """Say hello!"""
-    click.echo(f"Hello, {name}!")
     
 @cli.command()
-@click.option('--name',default='world',help='this a function to say bye')
-def bye(name):
-    click.echo(f"Bye {name}, It was a nice time having you!")
+@click.argument("file",type=click.Path(exists=True))
+@click.option("--output", default="cleaned.csv", help="Output file path")
+@click.option("--dropna",is_flag=True,help="Drop the rows with missing values")
+@click.option("--dropdupe",is_flag=True,help="Drop duplicate rows")
+@click.option("--fix-cols",is_flag=True,help="Standardize column names")
+def clean(file,output,dropna,dropdupe,fix_cols):
+    clean_csv(file,output,dropna,dropdupe,fix_cols)
+    click.echo(f"File cleaned succesfully and saved to {output}")
     
 
 if __name__ == "__main__":
