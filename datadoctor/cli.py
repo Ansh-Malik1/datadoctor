1cfef44193a3eb0210011c080cdfd96815b5bfe9
Ansh Malik <anshmalik718@gmail.com>
2025-07-24 15:09:22 +0530

    initialised cli structure and placeholder for scaler.py

--- datadoctor/cli.py
+++ datadoctor/cli.py
@@ -1,6 +1,7 @@
 import click
 from datadoctor.cleaner import clean_csv
 from datadoctor.encoder import encode_columns
+from datadoctor.scaler import scale_columns
 @click.group()
 def cli():
     """Datadoctor CLI"""
@@ -18,11 +19,19 @@ def clean(file,output,dropna,dropdupe,fix_cols):
 
 @cli.command()
 @click.argument("file",type=click.Path(exists=True))
-@click.option("--output", default="encoded_output.csv", help="Output file path")
+@click.option("--output","-o", default="encoded_output.csv", help="Output file path")
 @click.option("--method",type=click.Choice(["label","onehot"]),default='label',help='Encoding method')
 def encode(file,method,output):
     encode_columns(file,output,method)
     click.echo(f"{method} encoding done successfully!")
 
+@cli.command()
+@click.argument("file",type=click.Path(exists=True))
+@click.option("--output","-o",default="scaled_output.csv",help="Output file path")
+@click.option("--method",type=click.Choice(["standard", "minmax"], case_sensitive=False), help="Scaling method: standard or minmax")
+@click.option("--columns", multiple=True, help="Optional: Specific columns to scale. If not provided, all numerical columns (excluding binary) will be scaled")
+def scale(file,output,method,columns):
+    scale_columns(file, method.lower(), output, list(columns) if columns else None)
+    click.echo(f"Columns scaled successfully using {method} scaling method!.")
 if __name__ == "__main__":
     cli()
---
+++ datadoctor/scaler.py
@@ -0,0 +1,11 @@
+'''
+This will be the file that will contain the code to scale columns, according to the plan,
+if user provides columns, then scaling will be applied on that only else scaling will be
+applied to all numerical columns except binary.
+There will 2 scaling method provided to the user. These are :
+1. MinMax scaling (scaled between 0 to 1)
+2. Standard Scaling (mean=0, standard deviation=1)
+'''
+
+def scale_columns():
+    return
