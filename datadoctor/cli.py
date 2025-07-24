fb8d2f24f7db37b7143424d1c9c3db67f60a91a7
Ansh Malik <anshmalik718@gmail.com>
2025-07-24 13:42:26 +0530

    Initialised CLI strucutre and placeholder for encoder.py

--- datadoctor/cli.py
+++ datadoctor/cli.py
@@ -1,5 +1,6 @@
 import click
 from datadoctor.cleaner import clean_csv
+from datadoctor.encoder import encode_columns
 @click.group()
 def cli():
     """Datadoctor CLI"""
@@ -7,14 +8,21 @@ def cli():
     
 @cli.command()
 @click.argument("file",type=click.Path(exists=True))
-@click.option("--output", default="cleaned.csv", help="Output file path")
+@click.option("--output","-o", default="cleaned.csv", help="Output file path")
 @click.option("--dropna",is_flag=True,help="Drop the rows with missing values")
 @click.option("--dropdupe",is_flag=True,help="Drop duplicate rows")
 @click.option("--fix-cols",is_flag=True,help="Standardize column names")
 def clean(file,output,dropna,dropdupe,fix_cols):
     clean_csv(file,output,dropna,dropdupe,fix_cols)
     click.echo(f"File cleaned succesfully and saved to {output}")
-    
+
+@cli.command()
+@click.argument("file",type=click.Path(exists=True))
+@click.option("--output", default="encoded_output.csv", help="Output file path")
+@click.option("--method",type=click.Choice(['label','onehot']),default='label',help='Encoding method')
+def encode(file,method):
+    encode_columns(file,method)
+    click.echo(f"{method} encoding done successfully!")
 
 if __name__ == "__main__":
     cli()
---
+++ datadoctor/encoder.py
@@ -0,0 +1,11 @@
+'''
+This is the file that will contain the code to perform column encoding.There will be 2 encoding method
+provided to the user. The methods will be:
+1. One hot encoding
+2. Label Encoding
+
+If the user fails to provide any method,then label encoding will be the default encoding option. 
+'''
+
+def encode_columns(file,method='label'):
+    return
